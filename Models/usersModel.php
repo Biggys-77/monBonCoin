9ad8c0addc8b086405a940ff93eab67c2b7a2c28
ShibWolf <96662662+ShibWolf@users.noreply.github.com>
2023-02-14 11:47:07 +0100

    with UsersModel

--- Models/usersModel.php
+++ Models/usersModel.php
@@ -2,3 +2,101 @@
 
 namespace Models;
 
+use PDO;
+use App\Db;
+
+class UsersModel extends Db{
+    ///////////////////////////////// CRUD ///////////////////////////
+
+    ///////////////////////////// Méthode de lecture READ /////////////////////////////
+
+    //Trouver tout les utilisateur
+
+    public static function findAll(){
+
+    $request = "SELECT * FROM users";
+    $response = self::getDb()->prepare($request);
+    $response->execute();
+
+    return $response->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    // Trouver un utilisateur par son id
+
+    /**
+     * Attend un id d'utilisateurs
+     * @param array id[int]
+     */
+    public static function findById($id){
+        $request = "SELECT * FROM users WHERE idUser = ?";
+        $response = self::getDb()->prepare($request);
+        $response->execute($id);
+
+        return $response->fetch(PDO::FETCH_ASSOC);
+    }
+
+    // Trouver un utilisateur par son login
+    public static function findByLogin($login){
+        $request = "SELECT * FROM users WHERE login = ?";
+        $response = self::getDb()->prepare($request);
+        $response->execute($login);
+
+        return $response->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    //exo recherche par id ou login
+    public static function findByIdOrLogin(array $data){
+        if(is_int($data[0])){
+            $request = "SELECT * FROM users WHERE idUser = ?";
+        }else{
+            $request = "SELECT * FROM users WHERE login = ?";
+        }
+        $response = self::getDb()->prepare($request);
+        $response->execute($data);
+
+        return $response->fetch(PDO::FETCH_ASSOC);
+    }
+
+    // Recherche par clé
+    /**
+     * Cette méthode permet de trouver un ou des utilisateurs par n'importe quel critère
+     * elle attend un tableau addociatif
+     * @param array $user["clé" => ["valeur"]]
+     */
+    public static function findBy(array $user){
+        $request = "SELECT * FROM users WHERE " . key($user) ."= ?";
+        $response = self::getDb()->prepare($request);
+        $response->execute(current($user));
+
+        return $response->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /////////////////////////////// Méthode d'écriture CREATE ////////////////////////////////
+
+    public static function create(array $data){
+        // $data est un tableau qui contient les valeurs à inserer en BDD
+        
+        $request = "INSERT INTO users (login, password, firstName, lastName, adress, cp, city) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        $response = self::getDb()->prepare($request);
+        $response->execute($data);
+    }
+
+    ////////////////////////////// UPDATE ////////////////////////////////////
+
+    // Modification d'un utilisateur
+    public static function update(array $data){
+        $request = "UPDATE users SET login = ?, password = ?, firstName = ?, lastName = ?, adress = ?, cp = ?, city = ? WHERE idUser = ?";
+        $response = self::getDb()->prepare($request);
+        $response->execute($data);
+    }
+
+    //////////////////////////////// DELETE ////////////////////////////////
+
+    // Méthode de supression
+    public static function delete(array $id){
+        $request = "DELETE FROM users WHERE idUser = ?";
+        $response = self::getDb()->prepare($request);
+
+        return $response->execute($id);
+    }
+}
--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
 
 use Models\AnnoncesModel;
+use Models\UsersModel;
 
 require_once('autoloader.php');
 
 // Tester la méthode findAll()
-$order = "price DESC";
-$annonces = AnnoncesModel::findALL(null, "LIMIT 2");
+// $order = "price DESC";
+// $annonces = AnnoncesModel::findALL(null, "LIMIT 2");
 
-var_dump($annonces);
+// var_dump($annonces);
 
 // $id = [2];
 // $annonces2 = AnnoncesModel::findById($id);
@@ -31,8 +32,40 @@ var_dump($annonces);
 // AnnoncesModel::create($data);
 
 // Teste de la méthode Update()
-$data = [2,"Tondeuse à gazon","maximum 250m², moteur éléctrique", 185, "tondeuse.jpg", 4];
-AnnoncesModel::update($data);
+// $data = [2,"Tondeuse à gazon","maximum 250m², moteur éléctrique", 185, "tondeuse.jpg", 4];
+// AnnoncesModel::update($data);
 
-$id = [4];
-AnnoncesModel::delete($id);
+// Teste de la méthode delete()
+// $id = [4];
+// AnnoncesModel::delete($id);
+
+// Teste des méthodes pour les users
+// var_dump(UsersModel::findALL());
+// $id = [2];
+// var_dump(UsersModel::findById($id));
+
+// $login = ["admin@admin.fr"];
+// var_dump(UsersModel::findByLogin($login));
+
+// $data = [4];
+// $resultat = UsersModel::findByIdOrLogin($data);
+// if(!$resultat){
+//     echo "Cet utilisateur n'est pas présent en base de donnée";
+// }
+
+// Teste de la méthode avec clé
+// $user = ['password' => ['1234']];
+// var_dump(UsersModel::findBy($user));
+
+//Test de la méthode creat() users
+$pass = password_hash("1234", PASSWORD_DEFAULT);
+// $data = ["tchosdfds@yeah.com", $pass, "tchdsffdsupi", "tfdsfdsou", "66 Rue ddfsfsde Paris", 77140, "Nemours"];
+// UsersModel::create($data);
+
+//Test de la mthode update() users
+
+// $data = ["romain@gmail.com", $pass, "Romain", "Cesarini", "40 rue des sablons", 77760, "Larchant", 2];
+// UsersModel::update($data);
+
+// Teste de la méhode delete
+// UsersModel::delete([4]);
