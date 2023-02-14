aca5270b3ca3476541e9c459ab28ef5e554d8e2c
ShibWolf <96662662+ShibWolf@users.noreply.github.com>
2023-02-14 14:58:11 +0100

    with Routeur and htacces

---
+++ App/Routeur.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App;
+
+class Routeur{
+    public function app(){
+        // On test le routeur
+        // echo "le routeur fonctionne";
+
+        // Récupérer l'url
+        $request = $_SERVER['REQUEST_URI'];
+        // echo $request;
+
+        // On suprime "/monboncoin/public" de $request
+        $nb =strlen(SITEBASE);
+        $request = substr($request, $nb);
+        // echo "<hr>";
+        // echo $request;
+        // On casse $request pour récupérer uniquement la route et demandé et pas les aram GET
+        $request = explode("?", $request);
+        $request = $request[0];
+        // echo $request;
+
+        // On définit les routes du projet
+        switch ($request){
+            case '':
+                echo "vous êtes sur la page d'accueil";
+                break;
+            case 'annonces':
+                echo "vous êtes sur la page annonces";
+                break;
+            case 'annonceDetail':
+                echo "vous êtes sur la page annoncesDetail";
+                break;
+            case 'annonceAjout':
+                echo "page création d'annonce";
+                break;
+            case 'annonceModif':
+                echo "page modification d'annonce";
+                break;
+            case 'annonceSupp':
+                echo "page de suppression d'annonce";
+                break;
+            case 'panier':
+                echo "page paner";
+                break;
+            case 'inscription':
+                echo "page d'inscription";
+                break;
+            case 'connexion':
+                echo "page de connexion";
+                break;
+            case 'deconnexion';
+                echo "page de deconnexion";
+                break;
+            case 'profil':
+                echo "page de profil";
+                break;
+            default:
+                echo "cette pasge n'existe pas";
+                break;
+        }
+    }
+}
